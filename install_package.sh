28e5a44f5c856bc3e85c0893969f7c09d8b4422f
Chris Fleming <me@chrisfleming.org>
2011-09-12 00:56:04 +0100

    Added some status

--- install_package.sh
+++ install_package.sh
@@ -23,11 +23,14 @@ sudo ruby setup.rb
 sudo ln -s /usr/bin/gem1.8 /usr/bin/gem
 rm rubygems-1.3.7.tgz
 
+
 # GET source
+echo "Getting OSM Source"
 cd  $SRC
 git clone git://git.openstreetmap.org/rails.git
 
 # Postgres Setup
+echo "Setting up postgres"
 sudo apt-get install postgresql-contrib libpq-dev
 sudo su postgres -c "/usr/bin/psql < $BASEDIR/setup_user_databases.sql" 
 sudo su postgres -c "/usr/bin/psql -d openstreetmap < /usr/share/postgresql/8.4/contrib/btree_gist.sql"
@@ -37,6 +40,7 @@ cp $BASEDIR/database.yml $SRC/rails/config/database.yml
 
 cd $SRC/rails
 # Get the GEMS Needed
+echo "Getting the gems installed"
 sudo gem install -v=2.3.14 rails 
 sudo gem install timecop
 sudo gem install pg
@@ -45,6 +49,7 @@ sudo gem install rack-openid
 sudo gem install oauth
 
 # Setup Rails Port
+echo "Setup the rails port"
 cd $SRC/rails
 cp config/example.application.yml config/application.yml
 sudo rake gems:install
